19df891b640730be046bee328cc1171b516caf35
Tadea Simunovic <tsimunoviczg@gmail.com>
2020-04-14 18:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function findMinAndRemove(array){
-
+   let min = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++){
+      if(array[i] < min){
+        min = array[i]
+        minIndex = i
+      }
+    }
+    array.splice(minIndex, 1);
+    return min;
 }
 
 function insertionSort(array){
